b2ce88bec1de0f8a6bcaa8b268c2af25b8bc8af6
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2021-05-11 15:23:34 -0700

    set up framework for TimeSplitButton

--- src/TimeSplitButton.js
+++ src/TimeSplitButton.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export default class TimeSplitButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+
+  handleClick(event) {
+    //push date to server
+    let date = new Date();
+  }
+
+  render() {
+    return (
+      <div>
+      <label>Time Split </label>
+      <button onClick={this.handleClick}>Time Split</button>
+      </div>
+    )
+  }
 }
