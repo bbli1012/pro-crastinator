a9cd5e748c8389f75fa1897bb46542bbb205ead6
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2021-05-12 18:27:04 -0700

    TimeSplitButton now updates parent state

--- src/components/TimeSplitButton.js
+++ src/components/TimeSplitButton.js
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function TimeSplitButton() {
-  const [date, setDate] = useState(false);
+export default function TimeSplitButton({date, action}) {
+  //for use with parent
+  /**
+  const [dates, setDate] = useState([]);
 
-  /** legacy for class components
-  handleClick() {
-    //push date to server
-    let date = new Date();
+  function handleTime() {
+    let newDate = new Date();
+    setDate([...dates, newDate]);
   }
   */
 
-  // note onclick may need to be passed from parent to update parent state
-
   return (
     <div>
-    <label>Time Split </label>
-    <label> Current split is {(date) ? date : "not set"} </label>
-    <button onClick={() => setDate(new Date())}>Time Split</button>
+    <label> Current split is {(date.length) ? date[date.length - 1].toString() : 'not set'} </label>
+    <button onClick={action}>Time Split</button>
     </div>
   )
 }
