f710ccfcd6d4aa8234c3707ba7e916b9dee8510b
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2021-05-12 22:42:25 -0700

    implemented clock

--- src/components/ElapasedTime.js
+++ src/components/ElapasedTime.js
@@ -6,9 +6,22 @@ export default function ElaspsedTime({date}) {
 
   const [now, setNow] = useState(new Date());
 
+  useEffect(() => {
+    var timerID = setInterval( () => tick(), 1000 );
+
+    return function cleanup() {
+        clearInterval(timerID);
+      };
+  });
+
+  function tick() {
+  setNow(new Date());
+  }
+
   return (
     <div>
       <h1>{date.length ? 'Time since last split' : 'Current Time is'}</h1>
+      <h1>{now}</h1>
     </div>
   );
 }
