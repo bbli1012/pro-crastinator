fbbe69312dc1f8fd93b8e72a448085808ccbaeca
erichen886 <erichen886@gmail.com>
2021-05-13 00:46:24 -0700

    basic app layout laid out with flexbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      {/* TODO: replace with header/ nav component */}
       <header className="header">
         <nav className="nav">
-
+          <ul>
+            {/* <li><a href=''>Stand-in</a></li> */}
+            <li>home</li>
+            <li>data</li>
+          </ul>
         </nav>
-        Pro-crastinator
+        <h1>Pro-crastinator</h1>
       </header>
       <div className="row">
-
+        {/* TODO: replace with button component */}
+        <div className="split-button">Split</div>
+        {/* TODO: replace with time display component */}
+        <div className="time-elapsed-display">Time</div>
       </div>
       <div className="row">
+        {/* TODO: replace with splits component */}
         <div className="splits-column">
-
+          <div>split1</div>
+          <div>split2</div>
+          <div>split3</div>
         </div>
+        {/* TODO: replace with average graph talk about what to use eg D3? */}
+        <div className="graph-column">Graph goes here</div>
       </div>
     </div>
   );
