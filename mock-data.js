3597bc302f98086be92cfa251c37deb87ffbeddb
erichen886 <erichen886@gmail.com>
2021-05-14 15:57:55 -0700

    creating mock data

---
+++ mock-data.js
@@ -0,0 +1,24 @@
+
+const createMockSplits = (numberOfSplits) => {
+  const timeInMs = {
+    sec: 1000,
+    min: 60000,
+    hr: 36000000,
+  };
+  const category = {
+    social:
+    productivity:
+
+  }
+  let start = Date.now();
+
+}
+
+const splits = [
+  {
+    start:
+    stop:
+    category:
+    label:
+  }
+]
