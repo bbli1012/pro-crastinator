9a148ea67f133d3395f791d742d66c30038d02c0
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2021-05-15 14:49:52 -0700

    implement sunburst data parser function

---
+++ src/utility/sunburstDataParser.js
@@ -0,0 +1,64 @@
+export function sunburstDataParser (splits) {
+  /**
+   * {name: category
+   * children: [
+   *   {name: label, value: sum of stop - start}
+   * ]]
+   * }}
+   */
+  let data = []
+  let labels = {};
+  let categories = {};
+
+  //loop through data
+  for (let i = 0; i < splits.length; i++) {
+    let labelName = splits[i].label;
+    if (labels[labelName] == undefined) {
+      labels[labelName] = splits[i].stop - splits[i].start;
+      categories[splits[i].category] = {};
+      categories[splits[i].category][labelName] = true;
+    } else {
+      labels[labelName] += splits[i].stop - splits[i].start;
+    }
+  }
+
+  for (let key in categories) {
+    let obj = {
+      name: key,
+      children: []
+    }
+    for (label in categories[key]) {
+      obj.children.push({name: label, value: labels[label]});
+    }
+    data.push(obj);
+  }
+
+  return data;
+}
+
+
+/*
+labels {
+  labelName: totalTime
+}
+
+categories {
+  category: {
+    labelNames: true
+  }
+}
+
+
+
+[
+  { category
+    children: [
+      {
+        name: label
+        value: totalTime
+      }
+    ]
+  }
+]
+
+*/
