7273d0c64e38384714e6170b0ef12f7a15dbb368
benli <ben@onetechnology.io>
2021-05-27 00:44:47 -0700

    setup for sunburst charts

---
+++ src/components/sunburstChart.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function sunburstChart() {
+    return (
+    <div id='chartdiv' style={{width: '100%', height: '500px'}} ></div>
+    );
+};
