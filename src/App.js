355ff725c6c6ec37dfc68ce69c1d8fbd27914e58
Eric Chen <erichen886@gmail.com>
2021-05-27 12:07:33 -0700

    Updated setSplit. Possibly look into using lodash or slice method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import React, { useState, useEffect } from 'react';
-import TimeSplitButton from './components/TimeSplitButton.js';
-import ElapsedTime from './components/ElapsedTime.js';
-import SplitsDisplay from './components/SplitsDisplay.js';
+import React, { useState, useEffect } from "react";
+import TimeSplitButton from "./components/TimeSplitButton.js";
+import ElapsedTime from "./components/ElapsedTime.js";
+import SplitsDisplay from "./components/SplitsDisplay.js";
 
 // TODO: remove after database implementation
-import { mockSplits, mockLabels } from './mock-data.js';
+import { mockSplits, mockLabels } from "./mock-data.js";
 
 const App = () => {
   const [dates, setDate] = useState([]);
   const [splits, setSplits] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     // TODO: change to database get request later
     setSplits(mockSplits);
   }, []);
@@ -40,15 +40,19 @@ const App = () => {
         {/* TODO: replace with button component */}
         <TimeSplitButton date={dates} action={handleTime} />
         {/* TODO: replace with time display component */}
-        <ElapsedTime date={dates}/>
+        <ElapsedTime date={dates} />
       </div>
       <div className="row">
-        <SplitsDisplay splits={splits} mockLabels={mockLabels} setSplits={setSplits}/>
+        <SplitsDisplay
+          splits={splits}
+          mockLabels={mockLabels}
+          setSplits={setSplits}
+        />
         {/* TODO: replace with average graph talk about what to use eg D3? */}
         <div className="graph-column">Graph goes here</div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SplitsDisplay.js
+++ src/components/SplitsDisplay.js
@@ -1,37 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const SplitsDisplay = ({ splits, mockLabels, setSplits }) => {
-
   const onChange = (e) => {
-    let splitIdx = e.target.className;
-    let newLabel = e.target.value;
-    //need to create deep copy of splits
-  }
+    let { className: splitIdx, value: newLabel } = e.target;
+    // TODO: need to create deep copy of splits? Might be more expensive
+    // TOD: there is a slice variation as well
+    let newSplits = splits.map((split, idx) => {
+      if (idx === Number(splitIdx)) {
+        return { ...split, label: newLabel };
+      } else {
+        return split;
+      }
+    });
+    setSplits(newSplits);
+  };
 
   return (
     <div className="splits-column">
       {splits.map((split, idx) => {
         return (
           <div className="row" key={idx}>
-            <div>
-              {split.start}
-            </div>
-            {split.label === 'not defined'
-              ? <>
-                  <input list="labels" placeholder={split.label} onChange={onChange} className={idx}/>
-                  <datalist id="labels">
-                    { mockLabels.map((label, idx) => <option value={label} key={idx} />) }
-                  </datalist>
-                </>
-              : <div className={ split.category }>
-                  { split.label }
-                </div>
-            }
+            <div>{split.start}</div>
+            {split.label === "not defined" ? (
+              <>
+                <input
+                  list="labels"
+                  placeholder={split.label}
+                  onChange={onChange}
+                  className={idx}
+                />
+                <datalist id="labels">
+                  {mockLabels.map((label, idx) => (
+                    <option value={label} key={idx} />
+                  ))}
+                </datalist>
+              </>
+            ) : (
+              <div className={split.category}>{split.label}</div>
+            )}
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 export default SplitsDisplay;
